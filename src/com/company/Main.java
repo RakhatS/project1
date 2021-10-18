25c0e799fc50dfe5f5f1bd4fb9f055cbaba2598b
RakhatS <rakhat.saparbaev@gmail.com>
2021-10-18 21:32:42 +0600

    Init

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,16 +6,18 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class Main extends Application{
 
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("app.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("app.fxml")));
         primaryStage.setTitle("Rakho");
         primaryStage.setScene(new Scene(root , 604, 411));
         primaryStage.show();
 
     }
-
+//g
     public static void main(String[] args) {
         launch(args);
     }
